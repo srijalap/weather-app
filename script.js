d7bd6469bfab667888023c51598fbfe592013388
Srijana Poudel <srijana.poudel@tuni.fi>
2022-04-15 12:36:33 +0300

    course_assignment added Tabs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const getRandomUserData= () =>{
+const getLatestMeasurement= () =>{
     fetch("http://webapi19sa-1.course.tamk.cloud/v1/weather")
     .then((data) => data.json())
     .then((data) => {
@@ -13,11 +13,27 @@ const getRandomUserData= () =>{
             tableRows += `<tr><td>${i+1}</td><td>${dateTime}</td><td>${measurementType}</td><td>${measuredValue}</td></tr>`;
         }
 
-        const table = document.querySelector('#table>tbody');
+        const table = document.querySelector('#table1>tbody');
         table.innerHTML += tableRows;
 
     });
 
 };
 
-getRandomUserData();
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  getLatestMeasurement();
+
+  
